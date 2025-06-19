15222d5b40411a74b8776acd4df79f4bd1332676
root <nitsanhonen@mail.tau.ac.il>
2025-06-19 19:48:46 +0000

    almost

--- kmeansmodule.c
+++ kmeansmodule.c
@@ -94,10 +94,327 @@ struct vector **convert_centroids(PyObject *py_centroids, int k, int num_cords){
     return arr;
 }
 
-struct vector **convert_clusters(PyObject *py_clusters){
-    return NULL;
+struct vector **convert_clusters(PyObject *py_clusters, int d, int k){
+    Py_ssize_t lst_size;
+    PyObject *py_cords;
+    PyObject *double_object;
+    struct vector *head_vec, *curr_vec;
+    struct cord *head_cord, *curr_cord;
+    struct vector **arr;
+    int n, i, j;
+    arr = malloc(k*sizeof(struct vector *));
+    if(arr==NULL) return NULL;
+    if (!PyList_Check(py_clusters)) return NULL;
+    lst_size = PyList_Size(py_clusters);
+    n = (int) lst_size;
+    if(n<=0 || d<=0) return NULL;
+    head_vec = malloc(sizeof(struct vector));
+    if(head_vec == NULL) return NULL;
+    curr_vec = head_vec;
+    head_cord = malloc(sizeof(struct cord));
+    if(head_cord == NULL){
+        free(head_vec);
+        return NULL;
+    }
+    curr_cord = head_cord;
+    curr_cord->next = NULL;
+    for(i=0; i<n; i++){
+        curr_vec->cords = head_cord;
+        py_cords = PyList_GetItem(py_cords, i);
+        if (!PyList_Check(py_cords)){
+            free_head_vec(head_vec);
+            return NULL;
+        }
+        for(j=0; j<d; j++){
+            double_object = PyList_GetItem(py_cords, j);
+            curr_cord->value = PyFloat_AsDouble(double_object);
+            if(j+1==d){
+                curr_cord->next = NULL;
+            }
+            else{
+                curr_cord->next = malloc(sizeof(struct cord));
+                if(curr_cord->next == NULL){
+                    free_head_vec(head_vec);
+                    return NULL;
+                }
+                curr_cord = curr_cord->next;
+            }
+        }
+        if(i+1<n){
+            curr_vec->next = malloc(sizeof(struct vector));
+            if(curr_vec->next == NULL){
+                free_head_vec(head_vec);
+                return NULL;
+            }
+            curr_vec = curr_vec->next;
+            curr_vec->next = NULL;
+            curr_vec->cords = NULL;
+            head_cord = malloc(sizeof(struct cord));
+            if(head_cord==NULL){
+                free_head_vec(head_vec);
+                return NULL;
+            }
+        }
+    }
+    arr[0] = head_vec;
+    for(i=1; i<k; i++){
+        arr[i] = NULL;
+    }
+    return arr;
+} 
+
+double dist(struct cord *p, struct cord *q)
+{   
+    double sum=0.0;
+    double temp;
     
+    while(p != NULL&& q != NULL){
+       temp =  p->value - q->value;
+       sum += pow(temp,2);
+       p = p->next; 
+       q = q->next;
+
+    }
+    return sqrt(sum); 
 }
+
+int e_convergence(struct vector **prev_centr, struct vector **curr_centr, int k, int e){
+    double curr_dist;
+    int i;
+    for(i = 0; i < k; i++){
+        curr_dist = dist(prev_centr[i]->cords, curr_centr[i]->cords);
+        if(curr_dist>=e){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int min_dist(struct vector *vec, struct vector **arr, int k){
+    double min_dist = 1e100;
+    int min_idx;
+    double curr_dist;
+    struct cord *cor = vec->cords;
+    int i;
+    for(i=0; i<k; i++){
+        curr_dist = dist(cor, arr[i]->cords);
+        if(curr_dist<min_dist) {
+            min_dist = curr_dist;
+            min_idx = i;
+        }
+    }
+    return min_idx;
+}
+
+struct vector *deep_copy_vector(struct vector *vec)
+{
+    struct cord *curr_cord, *new_cord, *prev_cord;
+    struct vector *new_vec = malloc(sizeof(struct vector));
+    if (new_vec == NULL) {
+        return NULL; 
+    }
+    new_vec->cords = NULL;
+    new_vec->next = NULL;
+
+    curr_cord = vec->cords;
+    new_cord = NULL;
+    prev_cord = NULL;
+    while(curr_cord != NULL)
+    {
+        if (new_vec->cords == NULL) {
+            new_vec->cords = malloc(sizeof(struct cord));
+            if (new_vec->cords == NULL) {
+                free(new_vec);
+                return NULL; 
+            }
+            new_cord = new_vec->cords;
+        } else{
+            new_cord = malloc(sizeof(struct cord));
+            if (new_cord == NULL) {
+                free_head_vec(new_vec);
+                return NULL; 
+            }
+            prev_cord->next = new_cord;
+        }
+        new_cord->value = curr_cord->value;
+        new_cord->next = NULL;
+        prev_cord = new_cord;
+        curr_cord = curr_cord->next;
+    }
+    return new_vec;
+} 
+
+struct vector **new_cent(struct vector **cnt_arr, struct vector **klst_arr,  int k, int d){
+    int i;
+    int j;
+    int b;
+    int a;
+    int ind, cnt;
+    struct cord *updated_cnt;
+    struct vector *curr_vec;
+    struct vector **updated_cnt_arr = malloc(k * sizeof(struct vector *));
+    if (updated_cnt_arr == NULL) {
+        return NULL;
+    }
+    
+    for (i = 0; i < k; i++) {
+        updated_cnt_arr[i] = deep_copy_vector(cnt_arr[i]);
+        if (updated_cnt_arr[i] == NULL) {
+            
+            for (j = 0; j < i; j++) {
+                free_head_vec(updated_cnt_arr[j]);
+            }
+            free(updated_cnt_arr);
+            return NULL;
+        }
+    }
+   
+    for ( j = 0; j < k; j++)
+    {
+        
+        double *arr_sum = calloc(d, sizeof(double)); /* get as args instead of creating a new one each time?*/
+        if (arr_sum==NULL){
+            
+            for (a = 0; a < k; a++) {
+                free_head_vec(updated_cnt_arr[a]);  
+            }
+            free(updated_cnt_arr);
+            return NULL;
+        }
+        
+        cnt = 0;
+        curr_vec = klst_arr[j];
+        while (curr_vec != NULL) {
+            struct cord *curr_cord = curr_vec->cords;
+            
+            for ( b = 0; b < d; b++) {
+                arr_sum[b] += curr_cord -> value;
+                curr_cord = curr_cord-> next; 
+            }
+
+           cnt++;
+           curr_vec = curr_vec->next;
+
+        }
+        
+        updated_cnt = updated_cnt_arr[j]->cords;
+        
+        for (ind = 0; ind < d; ind++) {
+            if (cnt>0)
+            {
+                updated_cnt->value = arr_sum[ind] / cnt;
+            
+            }
+           updated_cnt = updated_cnt->next;
+        }
+
+        free(arr_sum);
+    }
+    return updated_cnt_arr;
+}
+    
+struct vector **re_kluster(struct vector **cent_arr, struct vector **org_klst_arr,  int k, int d){
+    int m;
+    int i;
+    int j;
+    int a;
+    struct vector *curr_vec, *new_vec, *head_vec;
+    struct vector **klst_arr = malloc(k*sizeof(struct vector *));
+    
+    (void)d; /*delete?*/
+    
+    if(klst_arr == NULL){
+        free(org_klst_arr);
+        return NULL;
+    }
+    
+    for (a = 0; a < k; a++) {
+        klst_arr[a] = NULL;
+    }
+    
+    for (i = 0; i < k; i++) {
+        head_vec = org_klst_arr[i];
+        curr_vec = head_vec;
+        while (curr_vec!=NULL)
+        { 
+            new_vec = deep_copy_vector(curr_vec);
+            if (new_vec == NULL) {
+                for (j = 0; j < i; j++) {
+                    free(klst_arr[j]);
+                }
+                free(klst_arr);
+                free(org_klst_arr);
+                return NULL;
+            }
+
+            /* min_dist(struct vector *vec, struct vector **arr, int k) - returns the index of the kluster that the vector needs to be reassigned to*/
+            m = min_dist(new_vec, cent_arr,k);
+            if(klst_arr[m] == NULL){
+                klst_arr[m] = new_vec;
+
+            }
+            else{
+                new_vec->next = klst_arr[m];
+                klst_arr[m] = new_vec;
+
+            }
+            curr_vec = curr_vec->next;
+        }
+        free_head_vec(head_vec);
+    }
+    free(org_klst_arr);
+    return klst_arr;
+}
+
+struct vector **kmeans_algo(struct vector **init_cent, struct vector **init_clust, int k, int d, int iter, int e){
+    struct vector **centroids;
+    struct vector **new_centroids;
+    struct vector **clusters;
+    int i, j;
+    int is_error = 0;
+
+    centroids = malloc(k*sizeof(struct vector));
+    if(centroids == NULL) return NULL;
+    for(i=0; i<k; i++){
+        centroids[i] = NULL;
+    }
+    new_centroids = init_cent;
+    clusters = init_clust;
+
+    for(i=0; i<iter; i++){
+        for(j=0; j<k; j++){
+            free_head_vec(centroids[j]);
+        }
+        free(centroids);
+        centroids = new_centroids;
+        clusters = re_kluster(centroids, clusters, k, d);
+        if(clusters == NULL){
+            is_error = 1;
+            goto free_mem;
+        }
+        new_centroids = new_cent(centroids, clusters, k, d);
+        if(new_centroids == NULL){
+            is_error=1;
+            goto free_mem;
+        }
+        if(e_convergence(centroids, new_centroids, k, e) == 1){
+            break;
+        }
+    }
+
+free_mem:
+    /*free all memory and exit*/
+    for(i=0; i<k; i++){
+        if(centroids != NULL) free_head_vec(centroids[i]);
+        if(clusters != NULL) free_head_vec(clusters[i]);
+    }
+    if(centroids != NULL) free(centroids);
+    if(clusters != NULL) free(clusters);
+
+    if(is_error==1) return NULL;
+    return new_centroids;
+}
+
 
 static PyObject* fit(PyObject *self, PyObject *args){
     PyObject* py_centroids;
@@ -105,12 +422,13 @@ static PyObject* fit(PyObject *self, PyObject *args){
     PyObject* py_cords;
     struct vector **centroids;
     struct vector **clusters;
+    struct vector **final_cent;
     Py_ssize_t cent_size;
     Py_ssize_t vec_size;
-    int k;
-    int d;
+    int k, d, i, e;
+    int iter;
 
-    if(!PyArg_ParseTuple(args, "OO", &py_centroids, &py_clusters)){
+    if(!PyArg_ParseTuple(args, "OOi", &py_centroids, &py_clusters, &iter, &e)){
         return NULL;
     }
 
@@ -124,7 +442,17 @@ static PyObject* fit(PyObject *self, PyObject *args){
     d = (int)vec_size;
     centroids = convert_centroids(py_centroids, k, d);
     if(centroids == NULL) return NULL;
-    clusters = convert_clusters(py_clusters);
+    clusters = convert_clusters(py_clusters, d, k);
+    if(clusters == NULL){
+        for(i=0; i<k; i++){
+            free_head_vec(centroids[i]);
+        }
+        free(centroids);
+        return NULL;
+    }
+    final_cent = kmeans_algo(centroids, clusters, k, d, iter, e);
+    if(final_cent == NULL) return NULL;
+    /*create func to convert final_cent to PyObject*/
 }
 
 static PyMethodDef kmeansMethods[] = {
