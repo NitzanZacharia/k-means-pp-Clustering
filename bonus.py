db5da6d232a5145c73ad79178a8f3f6e0d3e24a9
NitzanZacharia <zacharia1@mail.tau.ac.il>
2025-07-01 15:58:57 +0000

    bonus v2

--- bonus.py
+++ bonus.py
@@ -16,9 +16,25 @@ def comp_inrt(x, clusts, cents):
     inertia = 0.0
     for i in range(len(x)):
         cent = cents[clusts[i]]
-        dist = np.sum(getDistance(x[i], cent))
+        dist = getDistance(x[i], cent)
         inertia += dist
     return inertia
+def find_elbow(k_vals, inrts):
+    k = np.array(k_vals)
+    y = np.array(inrts)
+    p1 = np.array([k[0], y[0]])
+    p2 = np.array([k[-1], y[-1]])
+    line_v = p2 - p1
+    line_n = line_v / np.linalg.norm(line_v)
+    distances = []
+    for i in range(len(k)):
+        point = np.array([k[i], y[i]])
+        vp1 = point - p1
+        proj = np.dot(vp1, line_n) * line_n
+        pdist= vp1 - proj
+        distances.append(np.linalg.norm(pdist))
+    return k[np.argmax(distances)]
+
 def main():
      
     data_set = load_iris()
@@ -32,8 +48,19 @@ def main():
         inertia = comp_inrt(data_arr, clusts, cents)
         inrt.append(inertia)
     k_axis = range(1,11)
+    elbow_k = find_elbow(k_axis, inrt)
     plt.figure(figsize=(8, 5))
     plt.plot(k_axis, inrt, marker='o')
     plt.xlabel("K")
     plt.ylabel("Average Dispersion")
     plt.title("Elbow Method for selection of Optimal 'K' clusters")
+    plt.axvline(x=elbow_k, color='r', linestyle='--')
+    plt.annotate(f"Elbow at k={elbow_k}", xy=(elbow_k, inertias[elbow_k - 1]),
+                 xytext=(elbow_k + 0.5, inrt[elbow_k - 1] + 100),
+                 arrowprops=dict(facecolor='red', shrink=0.05))
+
+    plt.tight_layout()
+    plt.savefig("elbow.png")
+
+if __name__ == "__main__":
+    main()
